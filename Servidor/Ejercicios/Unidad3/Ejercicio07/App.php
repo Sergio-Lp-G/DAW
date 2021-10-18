300ce271d3e40f5d284b86b4627a980f696233ef
Sergio-Lp-G <sergiolpgar@gmail.com>
2021-10-18 19:24:11 +0100

    servidor 18/10/2021

--- Servidor/Ejercicios/Unidad3/Ejercicio07/App.php
+++ Servidor/Ejercicios/Unidad3/Ejercicio07/App.php
@@ -11,17 +11,67 @@ class App
         $this->student = "Sergio López García";
     }
 
+    public function run()
+    {
+        // Recoger los argumentos de $_GET
+        // El método a ejecutar depende de un argumento $GET 
+        if (isset($_GET['method'])) {
+            $method = $_GET['method'];
+        } else {
+            //La primera vez ejecuta el método index
+            //$method = 'index';
+            $method = 'fibonacci';
+        }
+
+        try {
+            $this->$method();
+        } catch (Throwable $th) {
+            if (method_exists($this, $method)) {
+                header("HTTP/1.0 500 Internal Server Error");
+                return http_response_code(500);
+                echo "Error en el servidor";
+            } else {
+                header("HTTP/1.0 404 Not Found");
+                echo "No encontrado";
+            }
+        } finally {
+            echo "<pre>";
+            print_r($th);
+        }
+    }
+
+
+
+    protected function index()
+    {
+        
+    }
+
     protected function numeros()
     {
-        $numvector=[];
+        $numvector = [];
     }
 
     public function fibonacci()
     {
+        $fibon=[0,1];
+        for($i=2;$i<100;$i++){
+            $fibon[]=$fibon[$i-1]+$fibon[$i-2];
+        }
+        foreach ($fibon as $key => $value) {
+            if($value < 1000000) {
+                echo $value."<br>";
+            }
+            
+        }
+
     }
 
     public function potencias2()
     {
+        for($i=2;$i<25;$i++){
+
+        }
     }
 
     public function factoriales()
