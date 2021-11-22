ba6418955dc778ef6428138a5517ff42f6040804
Sergio-Lp-G <sergiolpgar@gmail.com>
2021-11-22 15:08:51 +0000

    gim

--- Gimnasio/js/gimnasio.js
+++ Gimnasio/js/gimnasio.js
@@ -17,7 +17,35 @@ function calcularFCM() {
 
 }
 
-function crearHorario() {
+function horarioFisio() {
+    var fisios = ["Tania", "Juan", "Marta", "Tania", "Juan", "Marta", "Tania", "Juan", "Marta", "Francisco"];
+    for (var j = 0; j < 5; j++) {
+        fisios.forEach((fisio, index) => {
+            var elemento = document.createElement("td");
+            var texto = document.createTextNode(fisio);
+            elemento.appendChild(texto);
+            var ind = (index + 1).toString();
+            var lista = document.getElementById("fisio" + ind);
+            lista.appendChild(elemento);
+        });
+    }
+}
+
+function horarioSala() {
+    var actividades = ["Spining", "Yoga", "Crossfit", "KickBoxing", "Baile de salón", "Kapoeira", "BodyPump"];
+    for (var i = 0; i < 6; i++) {
+        actividades.forEach((dep, index) => {
+            var elemento = document.createElement("td");
+            var texto = document.createTextNode(dep)
+            elemento.appendChild(texto);
+            var ind = (index + 1).toString();
+            var lista = document.getElementById("actividad" + ind);
+            lista.appendChild(elemento);
+        });
+    }
+}
+
+function horarios() {
     var actividades = ["Spining", "Yoga", "Crossfit", "KickBoxing", "Baile de salón", "Kapoeira", "BodyPump"];
     for (var i = 0; i < 6; i++) {
         actividades.forEach((dep, index) => {
