99ac803bc2e51f2aa4e7e7ab3f67430bb9f3124a
Sergio-Lp-G <sergiolpgar@gmail.com>
2021-12-01 15:39:10 +0000

    javadock tv

---
+++ Despliegue/televisiones/tv.java
@@ -0,0 +1,46 @@
+//metodos y parametros de una tv:
+
+public class Tv {
+    String marca;
+    float pulgadas;
+    int volumen;
+    int canal;
+    String[] conexion;
+    boolean estado;
+    String[] configurar;
+
+    public tv (String marca; float pulgadas, int volumen, int canal,String[] conexion, boolean estado, String[] configurar){
+        this.marca=marca;
+        this.pulgadas = pulgadas;
+        this.volumen= volumen;
+        this.canal=canal;
+        this.conexion= conexion;
+        this.estado= estado;
+        this.configurar= configurar;
+    }
+
+    public void info(){
+        this.estado=false;
+        return estado;
+    }
+
+    public void a(){
+        
+    }
+
+    public void a(){
+        
+    }
+
+    public void a(){
+        
+    }
+
+    public void a(){
+        
+    }
+
+    public void a(){
+        
+    }
+}
